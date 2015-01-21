47e245ce77464506e4f0c89f5a61000e0f370574
moller-g-peter <=>
2015-01-22 00:49:40 +0100

    menus

--- js/sekai-net.js
+++ js/sekai-net.js
@@ -1,10 +1,17 @@
 // Wait for the DOM to load then run start
 $(function(){
-	$("section[class^='backEnd']").hide();
-
+	// $("section[class*='backEnd']").hide();
+	// $(".back").hide();
+	console.log( $(".frontEnd"));
   $(".login-btn").click(function() {
+	$("section[class*='frontEnd']").hide();
+	$("section[class*='backEnd']").show();
 
-    $("section").hide();
-	$("main section:last-child").show();
+    // $(".front").hide();
+    // $(".back").show();
+
+
+ //    $("header section:last-child").show();
+	// $("main section:last-child").show();
   });
 });
