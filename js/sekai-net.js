bb03e349d92195db2ff757ab8f811903fa0005d5
moller.peter <moller.g.peter@gmail.com>
2015-01-27 11:51:45 +0100

    modifying for db update

--- js/sekai-net.js
+++ js/sekai-net.js
@@ -1,63 +1,50 @@
-// Wait for the DOM to load then run start
-
-
 
 $(function(){
 	$("section[class*='backEnd']").hide();
-	// console.log( $(".frontEnd"));
 
   $(".login-btn").click(function() {
-	$("section[class*='frontEnd']").hide();
-	$("section[class*='backEnd']").show();
-	$(".clock").show();
+    $("section[class*='frontEnd']").hide();
+    $("section[class*='backEnd']").show();
+    $(".clock").show();
 
- //    $("header section:last-child").show();
+  // $("header section:last-child").show();
 	// $("main section:last-child").show();
   });
 
+  //------------------------------------------------------------------------
+
   $(".inputField").submit(function() {
-    //prepare adminFormData to be sent with AJAX
+
     var inputFieldData = {
       ":title" : $(this).find("#menu_inputField").val(),
-      ":body" : $(this).find("#pages_inputField").val(),
-      ":path" : $(this).find("#url_inputField").val()
-      //":user_id" : 1 //this has been moved to PHP
+      ":body" : $(this).find("#menu_textArea").val()
     };
 
-    //send adminFormData with AJAX to DB
-    insertNewPage(inputFieldData);
+    insert_text_to_DB(inputFieldData);
+ // console.log("what happens");
+    this.reset();
 
-    //empty the form once we're done with the information in it
-    this.reset(); //.reset() is a JS function, NOT a jQuery function... :D
-
-    //return false to prevent page reload on form submit
     return false;
   });
 
+  //------------------------------------------------------------------------
+
+  function insert_text_to_DB(inputFieldData) {
+    $.ajax({
+      url: "php/save_content.php",
+      type: "post",
+      dataType: "json",
+      data: {
+        "page_data" : inputFieldData
+      },
+      success: function(data) {
+        console.log("insert_text_to_DB success: ", data);
+      },
+      error: function(data) {
+        console.log("insert_text_to_DB error: ", data);
+      }
+    });
+  }
+
   
-
-  //function to insert a new page into the DB
-function insertNewPage(adminFormData) {
-  $.ajax({
-    url: "php/save_content.php",
-    type: "post",
-    dataType: "json",
-    data: {
-      //this request must have data to get through the 
-      //if-statement in save_content.php
-      "page_data" : adminFormData
-    },
-    success: function(data) {
-      console.log("insertNewPage success: ", data);
-      //on success, goTo() the contentList url
-      goTo("content-list");
-    },
-    error: function(data) {
-      console.log("insertNewPage error: ", data);
-    }
-  });
-}
-
-
-
 });
--- js/send_AJAX.js
+++ js/send_AJAX.js
@@ -1,59 +1,59 @@
-$(function(){
+// $(function(){
 
-	$(".backEndMain form").submit(function(){
-		var all_inputFields = {
-			":title" : $(this).find("#menu_inputField").val(),
-			":body" : $(this).find("#menu_textArea").val()
-		};
+// 	$(".backEndMain form").submit(function(){
+// 		var all_inputFields = {
+// 			":title" : $(this).find("#menu_inputField").val(),
+// 			":body" : $(this).find("#menu_textArea").val()
+// 		};
 
-		ajax_to_DB(all_inputFields);
+// 		ajax_to_DB(all_inputFields);
 
-		$(this).reset();
+// 		$(this).reset();
 
-		return false;
-	});
+// 		return false;
+// 	});
 
-	function ajax_to_DB(all_inputFields) {
-		$.ajax({
-			url: "php/save_content.php",
-			type: "post",
-			dataType: "json",
-			data: {
-			//this request must have data to get through the
-			//if-statement in save_content.php
-			"page_data" : all_inputFields
-		},
-		success: function(data) {
-			console.log("ajax_to_DB success: ", data);
-			//on success, click the .contentListLink class to
-			//switch to the contentList and getPages again
-			$(".contentListLink").click();
-		},
-		error: function(data) {
-			console.log("ajax_to_DB error: ", data);
-		}
-	});
-	}
+// 	function ajax_to_DB(all_inputFields) {
+// 		$.ajax({
+// 			url: "php/save_content.php",
+// 			type: "post",
+// 			dataType: "json",
+// 			data: {
+// 			//this request must have data to get through the
+// 			//if-statement in save_content.php
+// 			"page_data" : all_inputFields
+// 		},
+// 		success: function(data) {
+// 			console.log("ajax_to_DB success: ", data);
+// 			//on success, click the .contentListLink class to
+// 			//switch to the contentList and getPages again
+// 			$(".contentListLink").click();
+// 		},
+// 		error: function(data) {
+// 			console.log("ajax_to_DB error: ", data);
+// 		}
+// 	});
+// 	}
 
-	function insertNewPage(adminFormData) {
-			$.ajax({
-				url: "php/save_content.php",
-				type: "post",
-				dataType: "json",
-				data: {
-				//this request must have data to get through the
-				//if-statement in save_content.php
-				"page_data" : adminFormData
-			},
-			success: function(data) {
-				console.log("insertNewPage success: ", data);
-				//on success, click the .contentListLink class to
-				//switch to the contentList and getPages again
-				$(".contentListLink").click();
-			},
-			error: function(data) {
-				console.log("insertNewPage error: ", data);
-			}
-		});
-	}
-});
+// 	function insertNewPage(adminFormData) {
+// 			$.ajax({
+// 				url: "php/save_content.php",
+// 				type: "post",
+// 				dataType: "json",
+// 				data: {
+// 				//this request must have data to get through the
+// 				//if-statement in save_content.php
+// 				"page_data" : adminFormData
+// 			},
+// 			success: function(data) {
+// 				console.log("insertNewPage success: ", data);
+// 				//on success, click the .contentListLink class to
+// 				//switch to the contentList and getPages again
+// 				$(".contentListLink").click();
+// 			},
+// 			error: function(data) {
+// 				console.log("insertNewPage error: ", data);
+// 			}
+// 		});
+// 	}
+// });
